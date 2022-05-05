548ae4677fe54bec8bb634f61d67b1a3130e25e0
Hazem Krimi <Hazem.Krimi@CieloCosta.com>
2022-05-05 07:46:20 +0000

    Fix action modal overflow
    Related work items: #38692, #41719

--- React/src/components/Actions/ActionModal.tsx
+++ React/src/components/Actions/ActionModal.tsx
@@ -170,7 +170,7 @@ const ActionModal = ({
             </Flex>
           </Flex>
         </ModalHeader>
-        <ModalBody h="calc(100% - 175px)" p="0">
+        <ModalBody h="calc(100% - 175px)" overflow="auto" px={['0', '2']}>
           <Stack h="full" spacing={4}>
             <Stack spacing={6}>
               <Stack spacing={4}>
