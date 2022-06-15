6b9fd5c2c5bf328917bd11feec75790335f2003c
Hazem Krimi <Hazem.Krimi@CieloCosta.com>
2022-06-15 11:25:20 +0000

    Make Monday first day of the week for date picker component
    Related work items: #46360, #46367

--- React/src/components/Forms/Datepicker.tsx
+++ React/src/components/Forms/Datepicker.tsx
@@ -98,6 +98,7 @@ const Datepicker = ({
                 </Text>
               ) : (
                 <ReactDatepicker
+                  calendarStartDay={1}
                   dateFormat="d MMM yyyy"
                   dateFormatCalendar="MMMM"
                   disabledKeyboardNavigation
