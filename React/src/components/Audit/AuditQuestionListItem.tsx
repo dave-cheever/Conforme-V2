16a133491ed1cbb7b5f752afe246f06f01c98002
Hazem Krimi <Hazem.Krimi@CieloCosta.com>
2022-07-04 20:56:24 +0000

    Change status position in question list item
    Related work items: #48309, #48318

--- React/src/components/Audit/AuditQuestionListItem.tsx
+++ React/src/components/Audit/AuditQuestionListItem.tsx
@@ -1,5 +1,5 @@
 import { EditIcon } from '@chakra-ui/icons';
-import { Box, HStack, Spacer, Stack, Text } from '@chakra-ui/react';
+import { HStack, Spacer, Stack, Text } from '@chakra-ui/react';
 import { capitalize } from 'lodash';
 import pluralize from 'pluralize';
 
@@ -20,6 +20,11 @@ const AuditQuestionListItem = ({ question, handleDelete }: { question: TQuestion
       <Stack flexGrow={1} spacing={2}>
         <Text fontSize="smm">{question.question}</Text>
         <HStack>
+          {question.questionsCategory?.useStatus && (
+            <Text color="auditItem.listItem.action.color" fontSize="ssm">
+              {capitalize(question.answer?.status)}
+            </Text>
+          )}
           <ActionsIcon fill="transparent" stroke="auditItem.listItem.action.icon" />
           <Text color="auditItem.listItem.action.color" fontSize="ssm">
             {numberOfActions} {pluralize('Action', numberOfActions)}
@@ -27,11 +32,6 @@ const AuditQuestionListItem = ({ question, handleDelete }: { question: TQuestion
         </HStack>
       </Stack>
       <HStack>
-        {question.questionsCategory?.useStatus && (
-          <Box mr={2}>
-            <Text fontWeight="bold">{capitalize(question.answer?.status)}</Text>
-          </Box>
-        )}
         {question.answer?.attachments?.map((attachment) => (
           <DocumentThumbnail document={attachment} key={attachment.id} />
         ))}
