b2a4ab7d7bf05acdf926370be50475838b73bdfd
Oussema Heni <oussema.heni@CieloCosta.com>
2022-08-24 12:48:21 +0000

    Remove walk item modal bottom margin for tablets
    Related work items: #50831, #55207

--- React/src/components/WalkItems/WalkItemModal.tsx
+++ React/src/components/WalkItems/WalkItemModal.tsx
@@ -137,7 +137,7 @@ const WalkItemModal = ({
 
   return (
     <>
-      <ModalContent bg="actionModal.bg" h={['auto', '100vh']} m="0" mb={[4, 4, 0]} overflow="hidden" p={[4, 6]} rounded="0">
+      <ModalContent bg="actionModal.bg" h={['auto', '100vh']} m="0" mb={[4, 0, 0]} overflow="hidden" p={[4, 6]} rounded="0">
         <ModalHeader alignItems="center" fontSize="xxl" fontWeight="bold" p="0">
           <Flex justifyContent="space-between">
             <Flex alignItems="center" fontSize={['14px', '24px']}>
