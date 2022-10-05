b8dbe64672d61d07552111b7f79b8549e442752b
Oussema Heni <oussema.heni@CieloCosta.com>
2022-10-05 06:25:18 +0000

    Increased mobile and tablet chat panel zIndex
    Related work items: #58541

--- React/src/components/ChatMobileAndTablet.tsx
+++ React/src/components/ChatMobileAndTablet.tsx
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react';
 import Chat from './Chat';
 
 const ChatMobileAndTablet = ({ component }: { component: 'audit' | 'response' }) => (
-  <Box bg="white" boxShadow="lg" position="fixed" right="18px" rounded="20px" top={['175px']} zIndex="2">
+  <Box bg="white" boxShadow="lg" position="fixed" right="18px" rounded="20px" top={['175px']} zIndex="9">
     <Chat component={component} />
   </Box>
 );
