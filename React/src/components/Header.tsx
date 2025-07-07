b963d7731c2216bdaf35763d75418d0c91a41f7c
Hassan Tahir <Hassan.Tahir@CieloCosta.com>
2025-07-07 13:57:33 +0000

    Merged PR 7497: 112932-duplicate-add-buttons-mobile-view
    Remove the context specific add button on mobile since it's acting as a duplicate.
    
    Related work items: #112932

--- React/src/components/Header.tsx
+++ React/src/components/Header.tsx
@@ -1,15 +1,12 @@
 import { useMemo } from 'react';
 
-import { Button, Divider, Flex, Text } from '@chakra-ui/react';
+import { Divider, Flex, Text } from '@chakra-ui/react';
 import { capitalize } from 'lodash';
 
-import { useAdminContext } from '../contexts/AdminProvider';
-import { useAppContext } from '../contexts/AppProvider';
 import { useFiltersContext } from '../contexts/FiltersProvider';
-import useConfig from '../hooks/useConfig';
 import useDevice from '../hooks/useDevice';
 import useNavigate from '../hooks/useNavigate';
-import { AddIcon, ArrowRight } from '../icons';
+import { ArrowRight } from '../icons';
 import Can from './can';
 import FilterButton from './FilterButton';
 
@@ -20,12 +17,9 @@ interface IHeader {
   pageLabel?: string;
 }
 
-function Header({ children, breadcrumbs, mobileBreadcrumbs, pageLabel }: IHeader) {
+function Header({ children, breadcrumbs, mobileBreadcrumbs }: IHeader) {
   const { usedFilters } = useFiltersContext();
-  const { isPathActive, navigateTo } = useNavigate();
-  const { setAdminModalState } = useAdminContext();
-  const { trackerAddItems, auditAddItems } = useConfig();
-  const { module } = useAppContext();
+  const { isPathActive } = useNavigate();
 
   const isAuditPage =
     isPathActive('/audits') ||
@@ -54,10 +48,6 @@ function Header({ children, breadcrumbs, mobileBreadcrumbs, pageLabel }: IHeader
   function isPathAllowed() {
     return !excludedPaths.includes(window.location.pathname);
   }
-  const item =
-    module?.type === 'audits'
-      ? auditAddItems.find((item) => item.label === pageLabel)
-      : trackerAddItems.find((item) => item.label === pageLabel);
 
   const device = useDevice();
   const breadCrumbs = useMemo(() => breadcrumbs, [device, breadcrumbs, mobileBreadcrumbs]);
@@ -100,35 +90,6 @@ function Header({ children, breadcrumbs, mobileBreadcrumbs, pageLabel }: IHeader
                   <Divider borderColor="gray.300" display={['none', 'block']} height="30px" ml={0} mr={5} mt={1} orientation="vertical" />
                 )}
 
-                <Button
-                  _hover={{ opacity: 0.7 }}
-                  aria-label="Add"
-                  bg="navigationTop.addButton"
-                  bottom={['78px', '0']}
-                  boxShadow={['0px 0px 80px rgba(49, 50, 51, 0.25)', 'none']}
-                  color="white"
-                  data-id="b5bf85567bbe"
-                  display={'flex'}
-                  flexShrink={0}
-                  fontSize={['12px', '14px']}
-                  fontWeight={'500'}
-                  h={['42px', '40px']}
-                  leftIcon={<AddIcon data-id="6cff50759b96" h={['10px', '16px']} stroke="navigationTop.addIcon" w={['10px', '16px']} />}
-                  ml={['0', '4']}
-                  mr={['6rem', '0']}
-                  onClick={() => {
-                    const targetUrl = item?.url === '/dashboards' ? '/admin/tracker-items' : item?.url;
-                    navigateTo(targetUrl || '');
-                    setAdminModalState('add');
-                  }}
-                  position={['fixed', 'relative']}
-                  right={['0', usedFilters.length > 0 ? '15' : '25']}
-                  rounded={['10px', '8px']}
-                  w={['auto']}
-                  zIndex={5}
-                >
-                  {`Add ${item?.label || ''}`}
-                </Button>
               </>
             )}
           />
