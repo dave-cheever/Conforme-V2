5d9fa669e1f45e385893ca1c3c90ccbfe8f9f4ec
Hassan Tahir <Hassan.Tahir@CieloCosta.com>
2025-07-07 09:38:08 +0000

    Merged PR 7499: 112929- Horizontal scroll on Other Settings page
    Fix the width for component to avoid horizontal scroll
    
    Related work items: #112929

--- React/src/pages/admin/settings.tsx
+++ React/src/pages/admin/settings.tsx
@@ -56,7 +56,7 @@ function Settings() {
         flexDirection="row"
         h={['calc(100% - 160px)', 'calc(100% - 85px)']}
         px="25px"
-        w={['full', 'calc(100vw - 80px)', 'calc(100vw - 240px)']}>
+        w="full">
         <Flex
           bg="white"
           borderRadius="20px"
