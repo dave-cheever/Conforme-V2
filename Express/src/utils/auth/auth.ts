14924dff1e3b88f9980660aee392f4d8b3a6ceb0
Ben Whitehouse <Ben.Whitehouse@CieloCosta.com>
2025-08-04 12:13:47 +0000

    Merged PR 7553: 113511 - login issues
    login issues
    
    Related work items: #113511

--- Express/src/utils/auth/auth.ts
+++ Express/src/utils/auth/auth.ts
@@ -3,7 +3,7 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { customSession } from "better-auth/plugins";
 import { isBefore } from "date-fns";
 import { MongoClient } from "mongodb";
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 import { Organizations, Users } from "app-models";
 import { GraphService } from "app-services";
@@ -90,20 +90,17 @@ export const auth = betterAuth({
           const tenantId = (microsoftProvider?.options as any)?.tenantId;
           const domain = new URL(clientUrl).host;
           const organization = await Organizations.customFindByDomain(domain);
-
           if (isBefore(new Date(organization.licenceExpirationDate), new Date())) {
             throw new APIError("BAD_REQUEST", {
               message: "Organization's licence expired",
             });
           }
 
-
           // Check if logged user is from allowed tenant or organization is open to all tenants
           if (!organization.allowedTenantsIds.includes('all') && !organization.allowedTenantsIds.includes(tenantId))
             throw new APIError("BAD_REQUEST", {
               message: "User from this tenant is not allowed",
             });
-          
           // Check if logged user belong to access group (if configured)
           if (organization.accessGroupId) {
 
@@ -119,11 +116,17 @@ export const auth = betterAuth({
               throw new APIError("BAD_REQUEST", {
               message: "User doesn't exist in Conforme AAD group",
             });
-          }
-          const existingDbUser = (await Users.customFindWithDetails({ selector: { email: user.email }, organization, caseInsensitive: true }))[0] || {};
 
+          }
+          let existingDbUser: Partial<IUser> = {};
+          try {
+             existingDbUser = (await Users.customFindWithDetails({ selector: { email: user.email }, organization, caseInsensitive: true }))[0] || {};
+          }
+          catch (error){
+            console.log('error finding existing user', error)
+          }
           const updatedOrganisationIds = (existingDbUser.organizationsIds || []).includes(organization._id) ? existingDbUser.organizationsIds : [ ...(existingDbUser.organizationsIds || []), organization._id ] as any;
-          const userId = existingDbUser?._id || uuid.v4().toString();
+          const userId = existingDbUser?._id || uuidv4();
           const enrichedUser = {
             ...user,
             imgUrl: `${getProtocol()}${process.env.API_URL}/files/photo/${userId}`,
